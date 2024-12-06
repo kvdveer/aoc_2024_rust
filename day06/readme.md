201ce1db4aacdd47c8ed7a5522f523fc0d77042d
Koert van der Veer <koert@ondergetekende.nl>
2024-12-06 11:16:46 +0100

    Optimize day 6 part 2

--- day06/readme.md
+++ day06/readme.md
@@ -6,7 +6,7 @@ This part is mostly a simple turtle-implementation with collision detection. The
 
 After that the visited cells are counted.
 
-Alorithmic complexity is O(n) where n is the number of cells visited.
+As the path length correlates to the area of the grid, the algorithmic complexity is O(n^2) where n is the grid width.
 
 ## Part 2
 
@@ -14,7 +14,7 @@ We now need to see how we can alter the map, to force the turtle into a loop.
 
 I took the brute-force approach, as that would yield the easiest code. I simply tried to add an obstacle in every empty spot, and see if the guard would visit a spot that they had already visited. This approach was not correct: a guard could re-visit a spot while walking in a different direction. That needn't be part of the loop. Instead of a boolean to see if the guard has visited a spot, I stored direction the guard was walking in. In theory, the guard could visit a spot alternatingly in two directions and disguise that they are in a loop that way, but it is extremely unlikely that that would happen for the entire loop.
 
-The algorithmic complexity is O(m^2*n) where n is the number of cells visited, and n the grid size. As the path length correlates to grid size, the complexity is O(n^4).
+We're re-running part 1 for each grid cell, so algorithmic complexity is O(n^4) where n is the grid width.
 
 ## Performance
 
@@ -26,3 +26,7 @@ Base line:
 | part1      | 31.212 µs | 31.501 µs|  31.831  |
 | part2      | 218.74 ms | 219.87 ms | 221.03  |
 | complete   | 232.88 ms | 236.87 ms | 241.08 ms   |
+
+### Optimization: only try obstacles where the guard has visited before
+
+Obstacles not on the guard's path will never influence the guard's path. We can skip those. This does not really decrease the algorithmic complexity, as path length is still correlated to the grid area. It does lead to a linear speedup, as we can skip a lot of unnecessary calculations.
--- day06/src/puzzle_part2.rs
+++ day06/src/puzzle_part2.rs
@@ -1,81 +1,80 @@
+use std::collections::HashSet;
+
 use aoc_grid::{Coordinate, Direction, Grid};
 
 use crate::puzzle_input::{MapElement, PuzzleInput};
 
-#[derive(Debug, PartialEq, Clone, Copy, Default)]
-enum Visited {
-    #[default]
-    NotVisited,
-    Visited,
+#[derive(Clone, Copy, Hash, PartialEq, Eq, Debug)]
+struct Guard {
+    position: Coordinate,
+    direction: Direction,
 }
 
-struct MapState {
-    map: Grid<MapElement>,
-    guard_position: Coordinate,
-    guard_direction: Direction,
-}
-
-impl MapState {
-    fn new(base_map: &Grid<MapElement>) -> Self {
+impl Guard {
+    fn new(guard_position: Coordinate, guard_direction: Direction) -> Self {
         Self {
-            // Remove the guard from the map
-            map: base_map.map(|&c| match c {
-                MapElement::Guard => MapElement::Empty,
-                _ => c,
-            }),
-
-            // Look up the guard position
-            guard_position: base_map
-                .iter_coordinates()
-                .find(|&c| base_map[c] == MapElement::Guard)
-                .expect("map should have a guard position"),
-
-            // Guard always starts facing up
-            guard_direction: Direction::Up,
+            position: guard_position,
+            direction: guard_direction,
         }
     }
 
-    fn does_loop(&self, extra_obstacle: Coordinate) -> bool {
-        let mut guard_position = self.guard_position;
-        let mut guard_direction = self.guard_direction;
-        let mut visited: Grid<Option<Direction>> =
-            Grid::new(self.map.width() as usize, self.map.height() as usize);
+    fn step(&mut self, map: &Grid<MapElement>, extra_obstacle: &Coordinate) -> bool {
+        let mut next_position = self.position + &self.direction;
+        let mut next_element = map.get(next_position);
 
-        loop {
-            let mut next_position = guard_position + &guard_direction;
-            let mut next_element = self.map.get(next_position);
-
-            while next_position == extra_obstacle || next_element == Some(&MapElement::Obstacle) {
-                guard_direction = guard_direction.clockwise_4();
-                next_position = guard_position + &guard_direction;
-                next_element = self.map.get(next_position);
-            }
-
-            // Guard walked off the map
-            if next_element.is_none() {
-                return false;
-            }
-
-            // Guard walked into a loop
-            if visited[next_position] == Some(guard_direction) {
-                return true;
-            };
-
-            // walk to the next step
-            visited[next_position] = Some(guard_direction);
-            guard_position = next_position;
+        while next_element == Some(&MapElement::Obstacle) || &next_position == extra_obstacle {
+            self.direction = self.direction.clockwise_4();
+            next_position = self.position + &self.direction;
+            next_element = map.get(next_position);
         }
+
+        self.position = next_position;
+
+        next_element.is_some() // Return if we're still on the map
+    }
+}
+
+fn does_loop(map: &Grid<MapElement>, mut guard: Guard, extra_obstacle: &Coordinate) -> bool {
+    let mut visited = Grid::<Option<Direction>>::new(map.width() as usize, map.height() as usize);
+    loop {
+        if !guard.step(map, extra_obstacle) {
+            // Guard walked off the map
+            return false;
+        }
+        if visited[guard.position] == Some(guard.direction) {
+            return true;
+        }
+
+        visited[guard.position] = Some(guard.direction);
     }
 }
 
 pub fn solve(input: &PuzzleInput) -> String {
-    let state = MapState::new(&input.map);
-
-    input
+    let guard_position = input
         .map
         .iter_coordinates()
-        .filter(|&coordinate| input.map[coordinate] == MapElement::Empty)
-        .filter(|&coordinate| state.does_loop(coordinate))
+        .find(|&c| input.map[c] == MapElement::Guard)
+        .expect("map should have a guard position");
+
+    // Build a list of locations that the guard has visited in an unaltered map
+    let mut guard = Guard::new(guard_position, Direction::Up);
+    let mut visited_positions = HashSet::with_capacity(input.map.width() as usize * 10);
+    let fake_obstacle = Coordinate::new(-2, -2);
+    while guard.step(&input.map, &fake_obstacle) {
+        if guard.position != guard_position {
+            visited_positions.insert(guard.position);
+        }
+    }
+
+    visited_positions
+        .iter()
+        .filter(|coordinate| {
+            does_loop(
+                &input.map,
+                Guard::new(guard_position, Direction::Up),
+                coordinate,
+            )
+        })
         .count()
         .to_string()
 }
@@ -88,8 +87,7 @@ mod tests {
 
     #[rstest]
     #[case::example_input(include_str!("../example_input.txt"), "6")]
-    #[ignore]
-    #[case::final_input( include_str!("../input.txt"), "UNSOLVED")]
+    #[case::final_input( include_str!("../input.txt"), "1670")]
     fn test_solve(#[case] input: &str, #[case] expected: &str) {
         let input = PuzzleInput::try_from(input).unwrap();
         assert_eq!(solve(&input), expected);
